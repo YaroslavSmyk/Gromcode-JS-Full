f6ba53fd7925ac24dd5e16ab6a3f312fcac4b9e4
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-03-24 18:14:42 +0200

    t1

--- lesson13/task3/getMinSquaredNumber.test.js
+++ lesson13/task3/getMinSquaredNumber.test.js
@@ -6,11 +6,11 @@ it('arr empty', () => {
 });
 
 it('arr string', () => {
-    const result = getMinSquaredNumber('dfv');
-    expect(result).toEqual(null);
-  });
+  const result = getMinSquaredNumber('dfv');
+  expect(result).toEqual(null);
+});
 
-  it('min arr * arr', () => {
-    const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
-    expect(result).toEqual(4);
-  });
+it('min arr * arr', () => {
+  const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
+  expect(result).toEqual(4);
+});

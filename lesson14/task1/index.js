15930dc2f000a913bd4f1ab228723c7fdc829743
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-03-25 16:03:50 +0200

    t1

---
+++ lesson14/task1/index.js
@@ -0,0 +1,10 @@
+let message = 'Just learn it!';
+
+export function sendMessage(name) {
+    const sender = 'Gromcode';
+    console.log(`${name}, ${message}. You ${sender}`);
+};
+
+export function setMessage(text) {
+    message = text;
+}

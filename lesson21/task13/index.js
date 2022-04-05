0ab569a385e6a4963380ac138c23c85c7a8eec7a
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 21:24:13 +0300

    t1

---
+++ lesson21/task13/index.js
@@ -0,0 +1,5 @@
+const getSection = num => {
+  const span = document.querySelector(`span[data-number="${num.toFixed()}"]`);
+  const parent = span.closest('.box');
+  return parent.getAttribute('data-section');
+};

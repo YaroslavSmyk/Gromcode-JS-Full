44cc9af8a82ec522b2f8a268d3e00121788d7959
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 19:19:31 +0300

    t1

--- lesson21/task5/index.js
+++ lesson21/task5/index.js
@@ -1,14 +1,4 @@
-
-// 1.Получить округленную в меньшую сторону сумму с 2-мя знаками после запятой в формате '$17.15.
-// 2.сначала reduce потом toFixed потом Math.floor
-
-function getTotalPrice(num) {
-  let result = '';
-  let res1 = num.reduce((el, sum) => el + sum)
-
-  result = Math.floor(res1 * 100) / 100;
-
-  return '$'+result;
-}
-
-getTotalPrice([10.353, 12, 15.943]);
+export const setTitle = text => {
+  const title = document.querySelector('.title');
+  title.textContent;
+} 

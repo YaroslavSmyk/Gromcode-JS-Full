0ae468119e161e8314c6f7e3bdce4513a7893fcb
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 18:01:41 +0300

    t6

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -1,11 +1,11 @@
 export { getTitleElement, getInputElement };
 const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
-  console.dir('titleElem');
+  console.dir(titleElem);
   return titleElem;
 };
 const getInputElement = () => {
   const inputElem = document.querySelector('input');
-  console.dir('inputElem');
+  console.dir(inputElem);
   return inputElem;
 };

511fb0384f5c6cafcc4f8e0507699cb455917788
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 20:51:24 +0300

    t2

--- lesson21/task11/index.js
+++ lesson21/task11/index.js
@@ -1,12 +1,12 @@
 export const manageClasses = () => {
   const one = document.querySelector('.one');
-  one.classList.add('selected');
+  one.classList.add('.selected');
   const two = document.querySelector('.two');
-  two.classList.remove('selected');
+  two.classList.remove('.selected');
   const three = document.querySelector('.three');
-  three.classList.toggle('three_done');
+  three.classList.toggle('.three_done');
   const four = document.querySelector('.four');
-  if (four.classList.contains('some-class')) {
-    four.rclassList.add('another-class');
+  if (four.classList.contains('.some-class')) {
+    four.rclassList.add('.another-class');
   }
 };

5176d5d40a72441295e253afb2e20ce9e4c9163d
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 18:20:39 +0300

    t1

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,15 +1,14 @@
-'use strict';
+export { getItemsList, getItemArray };
 
-const multiRound = num => {
-  let result = [];
-  result.push(Math.floor(num * 100) / 100);
-  result.push(Math.trunc(num * 100) / 100);
-  result.push(Math.ceil(num * 100) / 100);
-  result.push(Math.round(num * 100) / 100);
-  result.push(+num.toFixed(2));
-  console.log(result);
+const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
 };
 
-// examples
-multiRound(11.12556); // ==> [11.12, 11.12, 11.13, 11.13, 11.13]
-multiRound(6.112); // ==> [6.11, 6.11, 6.12, 6.11, 6.11]
+const getItemArray = () => {
+  const eleventsArray = document.querySelectorAll('.tool');
+  console.dir(eleventsArray);
+return Array.from(eleventsArray);
+};
+

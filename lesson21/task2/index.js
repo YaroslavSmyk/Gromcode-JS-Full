7b6494c0ab4e44f3515474b0f0cac8202af6583f
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-05 17:45:56 +0300

    test1

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -1,28 +1,11 @@
-function getParsedIntegers(arr) {
-  let result1 = arr.map(num => Number.parseInt(num));
-  console.log(result1);
-  return result1;
-}
+const getTitleElement = () => {
+  const res1 = document.querySelector('.titile');
+  console.dir('titleElem')
+  return res1;
+} 
 
-function getParsedIntegersV2(arr) {
-  let result2 = arr.map(num => parseInt(num));
-  console.log(result2);
-  return result2;
-}
-
-function getParsedFloats(arr) {
-    let result3 = arr.map(num => Number.parseFloat(num));
-    console.log(result3);
-    return result3;
-}
-
-function getParsedFloatsV2(arr) {
-    let result4 = arr.map(num => parseFloat(num));
-    console.log(result4);
-    return result4;
-}
-
-getParsedIntegers([2, 3, '6.19']);
-getParsedIntegersV2([2, 3, '6text']);
-getParsedFloats([2, 3, '6text']);
-getParsedFloatsV2([2, 3, '6text']);
+const getInputElement = () => {
+  const res2 = document.querySelector('text')
+  console.dir('inputElem')
+return res2;
+};

7a0378030f7973bcc753d3446f5a8025fcf0cc6e
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-06 18:56:21 +0300

    t1

--- lesson22/task5/index.js
+++ lesson22/task5/index.js
@@ -1,4 +1,8 @@
-export const setTitle = text => {
-  const title = document.querySelector('.title');
-  title.textContent = text;
+const button = document.querySelector('.search__btn');
+
+const create = () => {
+  const input = document.querySelector('.search__input');
+  console.log(input.value);
 };
+
+button.addEventListener('click', create);

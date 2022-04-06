52641f9ba16dc515b65fac7874a7194c2a0224f8
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-06 20:11:56 +0300

    t1

--- lesson22/task9/index.js
+++ lesson22/task9/index.js
@@ -1,19 +1,11 @@
-export const finishList = () => {
-  const li1 = document.createElement('li');
-  li1.textContent = 1;
-  const li4 = document.createElement('li');
-  li4.textContent = 4;
-  const li6 = document.createElement('li');
-  li6.textContent = 6;
-  const li8 = document.createElement('li');
-  li8.textContent = 8;
+const task = document.querySelector('.task-status');
 
-  const ul = document.querySelector('.list');
-
-  ul.prepend(li1);
-  ul.append(li8);
-
-  const special = document.querySelector('.special');
-  special.before(li4);
-  special.after(li6);
+const create = event => {
+  if (event.target.checked) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 };
+
+task.addEventListener('change', create);

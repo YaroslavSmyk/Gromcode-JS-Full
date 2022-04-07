34d0a1be2b357463c2b0c1be4538ed8ceaaf3d95
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-07 19:57:16 +0300

    t2

--- lesson23/task2/index.js
+++ lesson23/task2/index.js
@@ -7,15 +7,14 @@ const generateNumbersRange = (from, to) => {
   return result;
 }
 
-const getLineSeats = () => {
-  return generateNumbersRange(1, 10)
+const getLineSeats = () => 
+generateNumbersRange(1, 10)
    .map(seatNumber => `
  <div 
      class="sector__seat"
      data-seat-number="${seatNumber}"
      ></div>
      `).join('');
- };
 
 const getSectorLines = () => {
   const seatString = getLineSeats();
@@ -25,21 +24,41 @@ const getSectorLines = () => {
 <div 
     class="sector__line"
     data-line-number="${lineNumber}"
-    >${seatString()}</div>
+    >${seatString}</div>
     `).join('');
 };
 
+const arenaElem = document.querySelector('.arena');
 
 const renderArena = () => {
-  const arenaElem = document.querySelector('.arena');
   const linesString = getSectorLines();
-const srctorsString = generateNumbersRange(1, 3)
+
+const sectorsString = generateNumbersRange(1, 3)
 .map(sectorNumber => `
 <div 
     class="sector"
     data-sector-number="${sectorNumber}"
-    >${linesString()}</div>
+    >${linesString}</div>
     `).join('');
     
-    arenaElem.innerHTML = srctorsString;
+    arenaElem.innerHTML = sectorsString;
+};
+
+const onSeatSelect = event => {
+  const isSeat = event.target.classList.contains('sector__seat')
+    if(!isSeat) {
+      return;
+    }
+
+const seatNumber = event.target.dataset.seatNumber;
+const lineNumber = event.target.closest('.sector__line').dataset.lineNumber
+const sectorNumber = event.target.closest('.sector').dataset.sectorNumber
+
+const selectedSeatElem = document.querySelector('.board__selected-seat');
+
+selectedSeatElem.textContent = ` S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`
 }
+
+arenaElem.addEventListener('click', onSeatSelect)
+
+renderArena();

5db8df1fcad6cbdd956114e45d07b7f7fd22bb05
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-11 17:48:48 +0300

    t1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,14 +1,8 @@
 export { dayOfWeek };
-const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+const week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
 const dayOfWeek = (date, days) => {
-  debugger;
   const day = new Date(date).getDate();
-  const dateInFuture = new Date(date).setDate(day + days);
-
-  return weekDays[new Date(dateInFuture).getDate()];
+  const resultDate = new Date(date).setDate(day + days);
+  return week[new Date(resultDate).getDay()];
 };
-
-const result = dayOfWeek(new Date(2019, 0, 1), 14);
-
-console.log(result);

4ac757ecc2b39f0462abcfc6ac44698c75227875
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-11 19:57:02 +0300

    t1

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,78 +1,27 @@
-const listElem = document.querySelector('.list');
+const getDiff = (startDate, endDate) => {
+  const a = Math.abs(new Date(startDate) - new Date(endDate));
 
-const tasks = [
-  { text: 'Buy milk', done: false, id: Math.random() },
-  { text: 'Pick up Tom from airport', done: false, id: Math.random() },
-  { text: 'Visit party', done: false, id: Math.random() },
-  { text: 'Visit doctor', done: true, id: Math.random() },
-  { text: 'Buy meat', done: true, id: Math.random() },
-];
+  const difference = Math.floor(a / 1000);
+  console.log('difference', difference);
+  const days = Math.floor(difference / 60 / 60 / 24);
+  console.log('days', days);
 
-const button = document.querySelector('.create-task-btn');
-const input = document.querySelector('.task-input');
-const todoList = document.querySelector('ul');
+  let leftsec = difference - days * 24 * 60 * 60;
+  console.log('leftsec', leftsec);
 
-const renderTasks = tasksList => {
-  const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.checked = done;
-      checkbox.classList.add('list__item-checkbox');
-      checkbox.dataset.id = id;
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      listItemElem.append(checkbox, text);
+  const hours = Math.floor(leftsec / 60 / 60);
+  console.log('hours', hours);
 
-      return listItemElem;
-    });
-  listElem.innerHTML = '';
-  listElem.append(...tasksElems);
+  leftsec = difference - days * 24 * 60 * 60 - hours * 60 * 60;
+  console.log('leftsec', leftsec);
+
+  const min = Math.floor(leftsec / 60);
+  console.log('min', min);
+
+  const sec = difference - days * 24 * 60 * 60 - hours * 60 * 60 - min * 60;
+  console.log('sec', sec);
+
+  return `${days}d ${hours}h ${min}m ${sec}s`;
 };
 
-const taskComplete = event => {
-  const isId = event.target.dataset.id;
-  console.log(isId);
-
-  const choseTask = tasks.find(elem => elem.id === +isId);
-
-  choseTask.done = !choseTask.done;
-
-  renderTasks(tasks);
-};
-
-todoList.addEventListener('click', taskComplete);
-const createElem = () => {
-  if (input.value === '') {
-    return;
-  }
-
-  tasks.push({ text: input.value, done: false, id: Math.random() });
-  renderTasks(tasks);
-  input.value = '';
-};
-
-button.addEventListener('click', createElem);
-
-renderTasks(tasks);
-
-
-
-// А) при нажатии на галочку(чекбокс) должино поменятся done в объекте и наоборот
-
-      
-// найти чекбокс? при галочке включить listItemElem.append(checkbox, text);
-
-
-
-// Б) при клике на Create создается новая запись в объекте:
-//   1) добавить текст из инпута
-//   2) поле пустое? да/ не довавляем в объект
-//   3) запись добавлена? да/ очистить инпут
-
-
-// В) задача done? да/задачу в низ списка.
+console.log(getDiff(new Date(2021, 2, 5, 0, 10, 5), new Date(2020, 1, 4, 11, 40, 10)));

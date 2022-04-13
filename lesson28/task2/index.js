11c1e676e6a4719deff6abc66a0d6594343398c4
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-13 13:56:08 +0300

    test1

---
+++ lesson28/task2/index.js
@@ -0,0 +1,27 @@
+// export { markFavorites };
+
+const favorites = ['id-2'];
+const tree = {
+  id: 'id-1',
+  name: 'Product',
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [],
+    },
+  ],
+};
+const markFavorites = (tree, favorites) => {
+  const isFavorites = favorites.includes(tree.id);
+
+  return {
+    ...tree,
+    isFavorites,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
+
+const result = markFavorites(tree, favorites);
+
+console.log(result);

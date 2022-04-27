a53fd15243c70ae5a23b126521920a3ac1f653d1
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-04-27 13:45:25 +0300

    test4

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -2,14 +2,16 @@ const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
-const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+const defaultAvatar = 'https://avatars.githubusercontent.com/u/2?v=4';
 userAvatarElem.src = defaultAvatar;
 
 const fetchUserData = userName => {
-  return fetch(`https://api.github.com/users/${userName}`).then(response => response.json());
+  return fetch(`https://api.github.com/users/${userName}`)
+  .then(response => response.json());
 };
 
 const renderUserData = userData => {
+  console.log(userData)
   const { avatar_url, name, location } = userData;
   userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
@@ -20,7 +22,7 @@ const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 
 const onSearchUser = () => {
-  const userName = userNameInputElem.ariaValueMax;
+  const userName = userNameInputElem.value;
   fetchUserData(userName).then(userData => renderUserData(userData));
 };
 
